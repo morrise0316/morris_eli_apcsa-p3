e7eef76b7de723729d36e22d60d566ad5c11d2b2
morrise0316 <eli.h.morris@gmail.com>
2019-02-01 12:59:49 -0800

    Input project update

--- Unit 00/src/Input.java
+++ Unit 00/src/Input.java
@@ -28,7 +28,32 @@ public class Input
 		System.out.print("Enter an integer :: ");
 		intTwo = keyboard.nextInt();
 
+		
+		
+		System.out.print("Enter a double :: ");
+		doubleOne = keyboard.nextDouble();
 
+
+		System.out.print("Enter a double :: ");
+		doubleTwo = keyboard.nextDouble();
+		
+		
+		
+		System.out.print("Enter a float :: ");
+		floatOne = keyboard.nextFloat();
+
+
+		System.out.print("Enter a float :: ");
+		floatTwo = keyboard.nextFloat();
+		
+		
+		
+		System.out.print("Enter a short :: ");
+		shortOne = keyboard.nextShort();
+
+
+		System.out.print("Enter a short :: ");
+		shortTwo = keyboard.nextShort();
 		//add in input for all variables
 
 
@@ -36,6 +61,14 @@ public class Input
 		System.out.println("integer one = " + intOne );
 		System.out.println("integer two = " + intTwo );
 
+		System.out.println("double one = " + doubleOne );
+		System.out.println("double two = " + doubleTwo );
+		
+		System.out.println("float one = " + floatOne );
+		System.out.println("float two = " + floatTwo );
+		
+		System.out.println("short one = " + shortOne );
+		System.out.println("short two = " + shortTwo );
 		//add in output for all variables
 
 
--- Unit 00/src/Variables.java
+++ Unit 00/src/Variables.java
@@ -23,26 +23,33 @@ public class Variables
         long longOne = 324532452;
 
 		//decimal variables
-        float floatOne = .2;
+        float floatOne = 0.2987f;
+        double doubleOne=.2244584534234;
 
 		//other integer types
-
+        char charOne = 103;
 
 		//other types
-
+        boolean boolOne = false;
+        String stringOne = "Comp Sci A";
 
 		//output your information here
 		System.out.println("/////////////////////////////////");
-		System.out.println("*                               *");
-		System.out.println("*        integer types          *");
-		System.out.println("*                               *");
-		System.out.println("*8 bit - byteOne = "+byteOne+"\t\t*");
-		System.out.println("*16 bit - shortOne = "+shortOne+"\t\t*");
-		System.out.println("*32 bit - intOne = "+intOne+"\t\t*");
-		System.out.println("*64 bit - longOne = "+longOne+"\t\t*");
-		System.out.println("*        real types             *");
-		
-
+		System.out.println("*                               ");
+		System.out.println("*       integer types           ");
+		System.out.println("*                               ");
+		System.out.println("*8 bit - byteOne = "+byteOne+"\t\t");
+		System.out.println("*16 bit - shortOne = "+shortOne+"\t\t");
+		System.out.println("*32 bit - intOne = "+intOne+"\t\t");
+		System.out.println("*64 bit - longOne = "+longOne+"\t\t");
+		System.out.println("*        real types             ");
+		System.out.println("*32 bit - floatOne = "+floatOne+"\t\t");
+		System.out.println("*64 bit - doubleOne = "+doubleOne+"\t\t");
+		System.out.println("*      other integer types      ");
+		System.out.println("*16 bit - charOne = "+charOne+"\t\t");
+		System.out.println("*        other types               ");
+		System.out.println("*boolOne = "+boolOne+"\t\t");
+		System.out.println("*stringOne = "+stringOne+"\t\t");
 
 
 
