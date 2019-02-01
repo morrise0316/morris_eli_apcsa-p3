fd7050730ba53699815d341c36a606b15ea961f2
morrise0316 <eli.h.morris@gmail.com>
2019-02-01 13:16:46 -0800

    Unit 01
    Finished the Stars and Stripes task.

---
+++ Unit 01/src/StarsRunner.java
@@ -0,0 +1,22 @@
+//© A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+public class StarsRunner
+{
+   public static void main(String args[])
+   {
+      //instantiate a StarsAndStripes object
+      StarsAndStripes myFlag = new StarsAndStripes();
+      //call the methods needed to make the patterns on the word document
+      myFlag.printASmallBox();
+      myFlag.printTwoBlankLines();
+      myFlag.printABigBox();
+   }
+}
